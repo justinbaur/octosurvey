1db70e40e1b466e19f2f354f14b61f6bc09c5873
justinbaur <silverhat@live.com>
2014-07-21 21:42:06 -0500

    comment

--- js/directives.js
+++ js/directives.js
@@ -9,34 +9,31 @@ angular.module('octosurvey.directives', []).
       elm.text(version);
     };
   }])
-  .directive('passwordVerify', function(){
-	return {
-		require: "ngModel",
-		scope: {
-		    passwordVerify: '='
-		},
-		link: function(scope, element, attrs, ctrl) {
-		    scope.$watch(function() {
-		        var combined;
-		        
-		        if (scope.passwordVerify || ctrl.$viewValue) {
-		           combined = scope.passwordVerify + '_' + ctrl.$viewValue; 
-		        }                    
-		        return combined;
-		    }, function(value) {
-		        if (value) {
-		            ctrl.$parsers.unshift(function(viewValue) {
-		                var origin = scope.passwordVerify;
-		                if (origin !== viewValue) {
-		                    ctrl.$setValidity("passwordVerify", false);
-		                    return undefined;
-		                } else {
-		                    ctrl.$setValidity("passwordVerify", true);
-		                    return viewValue;
-		                }
-		            });
-		        }
-		    });
-		}
-	};
-  });
+.directive('equals', function() {
+  return {
+    restrict: 'A', // only activate on element attribute
+    require: '?ngModel', // get a hold of NgModelController
+    link: function(scope, elem, attrs, ngModel) {
+      if(!ngModel) return; // do nothing if no ng-model
+
+      // watch own value and re-validate on change
+      scope.$watch(attrs.ngModel, function() {
+        validate();
+      });
+
+      // observe the other value and re-validate on change
+      attrs.$observe('equals', function (val) {
+        validate();
+      });
+
+      var validate = function() {
+        // values
+        var val1 = ngModel.$viewValue;
+        var val2 = attrs.equals;
+
+        // set validity
+        ngModel.$setValidity('equals', val1 === val2);
+      };
+    }
+  }
+});
