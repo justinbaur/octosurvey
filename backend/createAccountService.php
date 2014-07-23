4c9cb7e90430cbc5a707d9389fbd0fc9ffeedbbc
justinbaur <silverhat@live.com>
2014-07-23 02:19:16 -0500

    comment

--- backend/createAccountService.php
+++ backend/createAccountService.php
@@ -1,11 +1,13 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST")
 	{
+		$account = $_POST["account"];
+		$data = $_POST["data"];
 		$username = $_POST["username"];
 		$password = $_POST["password"];
 		$confirmpassword = $_POST["confirm_password"];
-			
-		$arr = array ('echo_username'=>$username,'echo_password'=>$password, 'echo_confirmpassword'=>$confirmpassword);
+		
+		$arr = array ('echo_account'=>$account, 'echo_data'=>$data, 'echo_username'=>$username,'echo_password'=>$password, 'echo_confirmpassword'=>$confirmpassword, 'echo_test'=>27);
 		echo json_encode($arr);
 	}
 ?>
