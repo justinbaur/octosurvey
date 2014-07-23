bf60e8a2f041664bcb712485ceb4449dde109c44
justinbaur <silverhat@live.com>
2014-07-23 01:42:27 -0500

    comment

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,18 @@ angular.module('octosurvey.controllers', [])
   .controller('HomeController', ['$scope', function($scope) {
 
   }])
-  .controller('CreateAccountController', ['$scope', function($scope) {
+  .controller('CreateAccountController', ['$scope','$http', function($scope, $http) {
 	$scope.createAccount = function(account) {
-
+		/* Call to PHP Service to insert into account */
+		$http.post('backend/createAccountService.php', account).
+			success(function(data, status){
+				$scope.data = data;
+				$scope.status = status;
+			}).
+			error(function(data, status){
+				$scope.data = data;
+				$scope.status = status;				
+			});
 	};
   }])
   .controller('MembershipController', ['$scope', function($scope) {
