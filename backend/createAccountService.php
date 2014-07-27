d76977af6e26462f68209f6353348f5cc6ad8594
justinbaur <silverhat@live.com>
2014-07-27 02:45:24 -0500

    comment

--- backend/createAccountService.php
+++ backend/createAccountService.php
@@ -1,12 +1,45 @@
 <?php
 	if($_SERVER["REQUEST_METHOD"] == "POST")
-	{
-		
+	{	
 		$account = json_decode(file_get_contents('php://input'), true);
-		$username = $account['username'];
-		$password = $account['password'];
+		$username = pg_escape_string($account['username']);
+		$password = pg_escape_string($account['password']);
 		
-		$arr = array ('echo_username'=>$username,'echo_password'=>$password, 'echo_test'=>27);
+		$dblogin = "fbktfkddiceiux";
+		$dbpass = "i3Xxbg_bH2bUOPy1EPJ1rT4PtH";
+		$dbname = "da113aib43h35h";
+		$dbhost = "ec2-54-197-237-120.compute-1.amazonaws.com:5432";
+
+		$connection = pg_connect("host='.$dbhost.' dbname='.$dbname.' user='.$dblogin.' password='.$dbpass.'") or die('Could not connect: '.pg_last_error());
+		
+		
+		
+		
+		
+		
+		/* E-mail Config 
+		$to = $username;
+		$subject = 'OctoSurvey Signup | Verification';
+		$message = '
+		
+		Thanks for signing up for OctoSurvey!
+		Your account has been created, you can login with the following credentials after you have activated your account via the URL.
+		
+		-----------------------
+		Username: '.$username.'
+		Password: '.$password.'
+		
+		
+		Please click the following link to activate your account:
+		https://octosurvey.herokuapp.com/verify.php?email='.$username.'&hash='.$hash.'
+		
+		';
+		
+		$headers = 'From:noreply@octosurvey.herokuapp.com' . "\r\n";
+		mail($to, $subject, $message, $headers);
+		*/
+		
+		$arr = array ('echo_username'=>$username,'echo_password'=>$password, 'echo_test'=>27, 'dbconfig'=>$_ENV['DATABASE_URL']);
 		echo json_encode($arr);
 	}
 ?>
