381607ba56d0263f8444a6e0530598134f7b9230
justinbaur <silverhat@live.com>
2014-07-27 17:35:42 -0500

    comment

--- backend/createAccountService.php
+++ backend/createAccountService.php
@@ -9,13 +9,13 @@
 		
 		#$connectionString = "host=ec2-54-197-237-120.compute-1.amazonaws.com port=5432 dbname=da113aib43h35h user=fbktfkddiceiux password=i3Xxbg_bH2bUOPy1EPJ1rT4PtH sslmode=require";
 
-		$dbHost = $dbUrl['host'];
-		$dbPort = $dbUrl['port'];
-		$dbName = $dbUrl['path'];
-		$dbUser	= $dbUrl['user'];
-		$dbPass = $dbUrl['pass'];
+		#$dbHost = $dbUrl['host'];
+		#$dbPort = $dbUrl['port'];
+		#$dbName = $dbUrl['path'];
+		#$dbUser	= $dbUrl['user'];
+		#$dbPass = $dbUrl['pass'];
 		
-		$db = pg_connect("host='.$dbHost.' port='.$dbPort.' dbname='.$dbName.' user='.$dbUser.' password='.$dbPass.' sslmode=require") or die('Could not connect: ' . pg_last_error());
+		#$db = pg_connect("host='.$dbHost.' port='.$dbPort.' dbname='.$dbName.' user='.$dbUser.' password='.$dbPass.' sslmode=require") or die('Could not connect: ' . pg_last_error());
 			
 		
 		
@@ -41,7 +41,7 @@
 		mail($to, $subject, $message, $headers);
 		*/
 		
-		$arr = array ('echo_username'=>$username,'echo_password'=>$password, 'echo_test'=>27, 'dbconfig'=>$_ENV['DATABASE_URL']);
-		echo json_encode($arr);
+		#$arr = array ('host'=>$dbHost,'port'=>$dbPort, 'dbName'=>$dbName, 'dbUser'=>$dbUser, 'dbPass'=>$dbPass);
+		echo json_encode($dbUrl);
 	}
 ?>
